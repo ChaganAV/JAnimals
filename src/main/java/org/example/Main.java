e23d7286107c7f7ee1242c5134ed38aaf0bbba75
Alexandr <jwchaganav@gmail.com>
2023-03-02 22:05:21 +0400

    Animal

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Животное");
+        System.out.println(animal);
+        Cat cat = new Cat("Вася");
+        System.out.println(cat);
+    }
+}
